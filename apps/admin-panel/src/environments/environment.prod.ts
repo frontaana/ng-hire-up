6c3c05438dcbaa424942fd4a1c1aaad8500cf146
Anastasia Andreeva <frontaana@gmail.com>
2026-05-04 19:45:47 +0300

    fav fixed

--- apps/admin-panel/src/environments/environment.prod.ts
+++ apps/admin-panel/src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 /**
  * Realtime Database  подходит для мгновенной синхронизации данных (real-time) и легко интегрируется через REST API
+ *
+ * apiSuffix: '.json'
  */
 export const environment = {
   production: true,
   apiBaseUrl: 'https://my-ng-admin-panel-default-rtdb.firebaseio.com',
-  // apiSuffix: '.json'
 };
