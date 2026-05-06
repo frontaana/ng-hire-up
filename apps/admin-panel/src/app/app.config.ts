0d744e3684f924ccf684996c76683ecc33384604
Anastasia Andreeva <frontaana@gmail.com>
2026-05-06 18:59:52 +0300

    added store packages

--- apps/admin-panel/src/app/app.config.ts
+++ apps/admin-panel/src/app/app.config.ts
@@ -2,8 +2,13 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin'; // Разработчики NGXS добавили слово Redux,
+import { provideStore } from '@ngxs/store';
 import { appRoutes } from './app.routes';
 import { firebaseInterceptor } from './services/interceptor/firebase.interceptor';
+// чтобы подчеркнуть совместимость с расширением браузера Redux DevTools,
+// и чтобы избежать путаницы с другими возможными плагинами отладки
+// import { QuestionsState } from './store/questions.state'; // создадим позже для NGXS
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +16,11 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([firebaseInterceptor])),
     provideRouter(appRoutes),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideStore(
+      [], // Сюда позже добавим NGXS стейты
+      withNgxsReduxDevtoolsPlugin()
+    ),
+    // Signal Store не требует глобальной регистрации,
+    // он работает через DI в компонентах или через providedIn: 'root'
   ],
 };
